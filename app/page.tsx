8ce10d710a6c30bf68e9c136bc4ec1d42b19edbe
Luke Davis <kule.visda@gmail.com>
2023-11-18 06:05:01 +0000

    Use web worker.

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@
 import { useState, useEffect } from "react";
 
 import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 import "react-pdf/dist/esm/Page/AnnotationLayer.css"
 import "react-pdf/dist/esm/Page/TextLayer.css";
